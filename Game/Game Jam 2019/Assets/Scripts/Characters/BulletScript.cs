507578033b03e8bcd00382caa56e65444842b911
Michael Cavataio <mcavatai@oswego.edu>
2019-06-21 23:14:12 -0400

    Adding missing files

---
+++ Game/Game Jam 2019/Assets/Scripts/Characters/BulletScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speedX = 0f;
+    public float speedY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x += speedX;
+        position.y += speedY;
+        transform.position = position;
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(0.75f);
+        Destroy(gameObject);
+    }
+}
